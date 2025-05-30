83d6f54088603a167f9532adbbdc48213760f370
DenisGhg <denisdenos5837@gmail.com>
2025-05-30 12:00:03 +0100

    Affichage de plus de détails sur un utilisateur dans un alertDialog

--- lib/pages/user_profile_with_random_user_api.dart
+++ lib/pages/user_profile_with_random_user_api.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:training_proj/widgets/app_text.dart';
 
 class UserProfileWithRandomUserApi extends StatefulWidget {
   const UserProfileWithRandomUserApi({super.key});
@@ -14,8 +15,9 @@ class _UserProfileWithRandomUserApiState extends State<UserProfileWithRandomUser
   //Utilisateurs récupérés depuis l'API
   List<dynamic> users = [];
 
+  //Fonction pour récuperer les users
   Future<void> fetchUsers() async {
-    final url = Uri.parse("https://randomuser.me/api/?results=20");
+    final url = Uri.parse("https://randomuser.me/api/?results=20&seed=api&nat=us");
     final client = http.Client();
     final response = await client.get(url);
 
@@ -43,8 +45,56 @@ class _UserProfileWithRandomUserApiState extends State<UserProfileWithRandomUser
           final name = "${user['name']['title']} ${user['name']['first']} ${user['name']['last']}";
           final email = user['email'];
           final pictureUrl = user['picture']['medium'];
+          final phoneNumber = user['phone'];
 
           return ListTile(
+
+            onTap: () {
+              // Plus de détails sur l'user
+              showDialog(
+                context: context,
+                builder: (context) => AlertDialog(
+                  title: AppText(text: "Profil", textAlign: TextAlign.center, fontSize: 25, color: Colors.blue, fontWeight: FontWeight.bold,),
+                  content: SizedBox(
+                    height: 300,
+                    child: Column(
+                      children: [
+                        //Image de profil
+                        SizedBox(
+                          height: 120,
+                          width: 120,
+                          child: CircleAvatar(
+                            backgroundImage: NetworkImage(pictureUrl),
+                          ),
+                        ),
+
+                        SizedBox(height: 25,),
+
+                        //Nom Prenom
+                        AppText(text: name),
+
+                        SizedBox(height: 25,),
+
+                        //Mail
+                        AppText(text: email),
+
+                        SizedBox(height: 25,),
+
+                        //Numero de téléphone
+                        AppText(text:  phoneNumber),
+                      ],
+                    ),
+                  ),
+                  actions: [
+                    TextButton(
+                      onPressed: () => Navigator.pop(context),
+                      child: Text("Fermer"),
+                    ),
+                  ],
+                ),
+              );
+            },
+
             leading: CircleAvatar(
               backgroundImage: NetworkImage(pictureUrl),
             ),
